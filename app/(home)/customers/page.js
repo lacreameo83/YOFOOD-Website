b10f1accf6ddd58dd8e920f06ea22b81f58858a2
precious osonwa <142677806+lacreameo83@users.noreply.github.com>
2024-12-04 10:54:04 +0100

    trying to build

--- app/(home)/customers/page.js
+++ app/(home)/customers/page.js
@@ -16,10 +16,10 @@ function Page() {
         <div className="w-[299px] sm:w-[570px] h-auto flex flex-col gap-[40px] sm:gap-0">
           <div className="  sm:w-[568px] flex flex-col  gap-7 h-[212px]">
             <p className="text-[#FFFFFF] text-start sm:text-left    Poppinsfonts font-[400] text-[13px] sm:text-[18px] ">
-              You won&apos;t find a restaurant in Greenland that serves such
-              amazing food. Their new menu surprised me. All new, never seen and
-              tasted such food before in my life. keep on creating new menus
-              I&apos;m really looking forward to it
+              {`" You won't find a restaurant in Greenland that serves
+              such amazing food. Their new menu surprised me. All new, never
+              seen and tasted such food before in my life. keep on creating new
+              menus I'm really looking forward to it  "`}
             </p>
             <div>
               <p className="text-[#FFFFFF]  font-[400] text-[13px] sm:text-[18px] leading-[17px] sm:leading-[27px] list-disc SerifTextfonts list-inside">
